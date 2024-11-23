d899c35e9027885f3563f7428c8658078c134d6c
LUALROAL <alejorodryguez@gmail.com>
2024-11-23 17:58:26 -0500

    creacion de componentes

--- vivero-verde-vida/src/app/app-routing.module.ts
+++ vivero-verde-vida/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { CatalogoComponent } from './features/catalogo/catalogo.component';
+import { ServiciosComponent } from './features/servicios/servicios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'catalog', component: CatalogoComponent },
+  { path: 'service', component: ServiciosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- vivero-verde-vida/src/app/app.module.ts
+++ vivero-verde-vida/src/app/app.module.ts
@@ -5,16 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
+import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatSliderModule,
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
